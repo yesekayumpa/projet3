caf7b0fb47d71847efdaa596362161fb784a2308
Yese KAYUMPA BLAISE <116753749+yesekayumpa@users.noreply.github.com>
2022-12-01 03:49:29 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php require_once("data.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/style.css">
+    <title>projet3</title>
+</head>
+<body>
+    <?php require_once("liste.auteurs.html.php")?>
+    <?php require_once("liste.rayons.html.php")?>
+    <?php require_once("liste.ouvrages.html.php")?>
+
+</body>
+</html>
